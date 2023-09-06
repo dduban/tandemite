15d5a22cc3fd3774f4b5b9c4835cc5a86eec4719
dduban <dominika.dubaniowska@gmail.com>
2023-09-06 10:13:19 +0200

    action for person form

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -2,20 +2,67 @@
 
 namespace App\Controller;
 
+use App\Entity\Person;
+use App\Form\PersonDataFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class PersonController extends AbstractController
 {
     /**
      * @Route("/person", name="app_person")
      */
-    public function index(): JsonResponse
+    public function formIndex(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/PersonController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/person/new", name="formNew")
+     */
+    public function new(Request $request, SluggerInterface $slugger): Response
+    {
+        $person = new Person();
+        $form = $this->createForm(PersonDataFormType::class, $person);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $file */
+            $file = $form->get('filePath')->getData();
+
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+                try {
+                    $file->move(
+                        $this->getParameter('files_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                $person->setFilePath($newFilename);
+            }
+
+            // ... persist the $product variable or any other work
+
+            return $this->redirectToRoute('app_person_list');
+        }
+
+        return $this->render('person-form.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
